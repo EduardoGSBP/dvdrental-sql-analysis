e0e53754a59b468bffe121ce396049f49c94f7c9
Eduardo Gabriel <122492208+EduardoGSBP@users.noreply.github.com>
2025-11-06 23:20:55 -0300

    Update 📁 05_categoria_popular.sql

--- queries/📁 05_categoria_popular.sql
+++ queries/📁 05_categoria_popular.sql
@@ -1,3 +1,15 @@
+05_categoria_popular.sql — Categorias de filmes mais populares
+
+📋 Descrição:
+Exibe as categorias de filmes com mais aluguéis, revelando os gêneros preferidos pelos clientes (ex: Ação, Comédia, Drama).
+Essas informações são úteis para orientar futuras aquisições de títulos e estratégias de recomendação.
+
+🔍 Principais conceitos:
+
+•JOIN entre category, film_category, film, inventory e rental
+•COUNT() para somar os aluguéis por categoria
+•GROUP BY e ORDER BY DESC para gerar o ranking
+_____________________________________________________________________________
 SELECT c.name AS categoria, COUNT(r.rental_id) AS total_alugueis
 FROM category c
 JOIN film_category fc ON c.category_id = fc.category_id
