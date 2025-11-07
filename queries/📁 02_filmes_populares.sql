854399df1f6b7a13ee12e46d86b438a2324b1ec1
Eduardo Gabriel <122492208+EduardoGSBP@users.noreply.github.com>
2025-11-06 23:18:44 -0300

    Update 📁 02_filmes_populares.sql

--- queries/📁 02_filmes_populares.sql
+++ queries/📁 02_filmes_populares.sql
@@ -1,3 +1,17 @@
+2️⃣ 02_filmes_populares.sql — Filmes mais alugados
+
+📋 Descrição:
+Mostra quais filmes tiveram o maior número de aluguéis, revelando os títulos mais procurados pelos clientes.
+Essas informações ajudam a entender as tendências de consumo e quais filmes devem ser mantidos em estoque.
+
+🔍 Principais conceitos:
+
+•JOIN entre film, inventory e rental
+•COUNT() para contar quantos aluguéis cada filme teve
+•GROUP BY para agrupar por título do filme
+•ORDER BY DESC para listar do mais popular ao menos
+  _____________________________________________________________
+  
 SELECT f.title, COUNT(r.rental_id) AS total_alugueis
 FROM film f
 JOIN inventory i ON f.film_id = i.film_id
