13a3b065bc361dad028878fd23ae2519be3f0240
Eduardo Gabriel <122492208+EduardoGSBP@users.noreply.github.com>
2025-11-06 23:20:24 -0300

    Update 📁 04_funcionarios.sql

--- queries/📁 04_funcionarios.sql
+++ queries/📁 04_funcionarios.sql
@@ -1,3 +1,15 @@
+04_funcionarios.sql — Funcionário que mais realizou atendimentos
+
+📋 Descrição:
+Mostra qual funcionário registrou o maior número de aluguéis, permitindo medir o desempenho individual da equipe.
+É uma forma prática de acompanhar produtividade e reconhecer os melhores colaboradores.
+
+🔍 Principais conceitos:
+
+•JOIN entre staff e rental
+•COUNT(rental_id) para contar quantos atendimentos cada funcionário fez
+•GROUP BY e ORDER BY DESC para ordenar pelo total de atendimentos
+___________________________________________________________________________________
 SELECT s.first_name, s.last_name, COUNT(p.payment_id) AS total_atendimentos
 FROM staff s
 JOIN payment p ON s.staff_id = p.staff_id
