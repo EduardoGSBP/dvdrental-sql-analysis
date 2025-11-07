eec6707b99b940f240fdc82637a5a16eabe03305
Eduardo Gabriel <122492208+EduardoGSBP@users.noreply.github.com>
2025-11-06 23:17:21 -0300

    Update 📁 01_clientes_ativos.sql

--- queries/📁 01_clientes_ativos.sql
+++ queries/📁 01_clientes_ativos.sql
@@ -1,3 +1,17 @@
+1️⃣ 01_clientes_ativos.sql — Top 10 clientes que mais alugam filmes
+
+📋 Descrição:
+Essa consulta identifica os clientes mais ativos da locadora, ou seja, aqueles que mais realizaram aluguéis.
+É útil para estratégias de fidelização e marketing, pois mostra quem são os clientes mais engajados.
+
+🔍 Principais conceitos:
+
+•JOIN entre customer e rental
+•COUNT() para contar o total de aluguéis
+•GROUP BY para agrupar por cliente
+•ORDER BY e LIMIT para exibir o ranking
+  __________________________________________________________
+  
 SELECT c.first_name, c.last_name, COUNT(r.rental_id) AS total_alugueis
 FROM customer c
 JOIN rental r ON c.customer_id = r.customer_id
